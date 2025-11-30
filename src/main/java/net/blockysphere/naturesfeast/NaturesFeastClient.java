469b9db556a4154cf5df002145430f73b3cf7129
Blocky_Sphere <ian.he.kkg@gmail.com>
2025-11-30 10:34:00 +0900

    Initial commit

---
+++ src/main/java/net/blockysphere/naturesfeast/NaturesFeastClient.java
@@ -0,0 +1,10 @@
+package net.blockysphere.naturesfeast;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NaturesFeastClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/blockysphere/naturesfeast/NaturesFeastDataGenerator.java
@@ -0,0 +1,11 @@
+package net.blockysphere.naturesfeast;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NaturesFeastDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
